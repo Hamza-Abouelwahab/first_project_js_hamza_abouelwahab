f4459f6af02ec36ab68d4c8257b18e53f3481cc1
hamza-133 <hamzabouelwahab04@gmail.com>
2025-12-29 14:21:40 +0100

    check name of users

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,50 @@
+class Info {
+    constructor(name, email, age, password) {
+        this.name = name
+        this.email = email
+        this.age = age
+        this.password = password
+    }
+}
+
+let databasse = []
+
 while (true) {
-    let askUser = prompt("What do you want now?\n- sign up\n- login\n- change password\n- exit")
+
+    let askUser = prompt("What do you want now?\n- sign up\n- login up\n- change password\n- exit")
 
     askUser = askUser.trim().toLowerCase()
 
     if (askUser === "exit") {
-        alert("menu")
+        alert("Exiting menu...")
         continue
     }
 
     if (askUser === "sign up") {
-        alert("chosen sign up")
+
+        // * Name
+        let uName = prompt("Insert your name")
+        function checkName(name) {
+            return name.trim().split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(" ")
+        }
+        
+        while (uName.replace(/\s+/g, "").length < 5 || !/^[a-zA-Z\s]+$/.test(uName)) {
+            alert("Write at least 5 letters and don't use numbers or special characters")
+            uName = prompt("Insert your name")
+        }
+        uName = checkName(uName)
+
+        let newUser = new Info(uName)
+        databasse.push(newUser)
+
+        alert("Welcome")
+        console.log(databasse)
+
     } else if (askUser === "login up") {
-        alert("chosen login")
+        alert("Chosen login")
     } else if (askUser === "change password") {
-        alert("chosen change password")
+        alert("Chosen change password")
     } else {
         alert("You are not serious")
     }
 }
-
